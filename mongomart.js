9e17943a6d0d5c5e235aee942aa2f0b86fc2f3c2
Georges BODIONG <deebodiong@gmail.com>
2017-08-26 17:33:14 +0100

    Update mongomart.js

--- mongomart.js
+++ mongomart.js
@@ -271,7 +271,7 @@ MongoClient.connect(uri, function(err, db) {
     
     // Start the server listening
     var server = app.listen(3000, function() {
-        var port = server.address().port;
+    var port = server.address().process.env.PORT;
         console.log('Mongomart server listening on port %s.', port);
     });
 
